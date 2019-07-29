b3c22b0b8525421fd1d69712bef4559b7af69689
Daniel Torres <danielfeelfine@gmail.com>
2019-07-29 12:54:23 -0300

    add: index with types

---
+++ index.js
@@ -0,0 +1,24 @@
+// @flow
+
+export type User = {
+  id: string
+}
+
+export type ErrorType = {
+  code: string,
+  message: string
+}
+
+export type Headers = {
+  authorization: String
+}
+
+export type FunctionConfig = {
+  region: string,
+  environment: string
+}
+
+export type ProviderType = 'NATIVE' | 'THIRDY'
+export type Provider = {
+  type: ProviderType
+}

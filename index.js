44e50dc3d64be4f154963262b4698fdc00007b43
Daniel Torres <danielfeelfine@gmail.com>
2019-07-29 19:42:06 -0300

    add: types of policies-worker

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
 // @flow
 
+export type Application = {
+  id: string
+}
+
+export type Agent = {
+  id: string
+}
+
+export type Provider = {
+  id: string
+}
+
+export type Contact = {
+  id: string
+}
+
 export type User = {
   id: string
 }
@@ -19,6 +35,10 @@ export type FunctionConfig = {
 }
 
 export type ProviderType = 'NATIVE' | 'THIRDY'
-export type Provider = {
-  type: ProviderType
+
+export type ErrorType = {
+  code: string,
+  message: string
 }
+
+export type Operation = "CREATE" | "UPDATE" | "DELETE" | "GET"
